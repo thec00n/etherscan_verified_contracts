666bb553af0e4fe4dfff826c66d6dcee3c9f8dab
thec00n <thec00n@fork.at>
2018-08-27 19:39:30 +0800

    updated contracts,README, add getContracts.js

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# etherscan_verified_contracts
+# Etherscan verified contracts
+
+This repository contains verfied contracts from Etherscan. 
+
+Run `getContracts.js` to slow crawl Etherscan and download the latest contracts. 
+
+Please update the repository with the newest contracts. Create a pr and I will merge it in asap. 
+
---
+++ contracts/0x168f4673768cfad58d43649cb9ffd33516cc1a13_Migrations.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.24;
+// produced by the Solididy File Flattener (c) David Appleton 2018
+// contact : <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="22464354476243494d4f40430c414d4f">[email protected]</a>
+// released under Apache 2.0 licence
+contract Migrations {
+    address public owner;
+    uint public last_completed_migration;
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) public restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
+}
